0bbcb5b9a7d5773d10ef597ad80790bb857915fb
Ben Greaves <bsgreaves@gmail.com>
2017-05-18 18:01:34 -0500

    Stopping to work on route routing

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,34 @@ app.run((FIREBASE_CONFIG) => {
 	firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-app.controller("helloCtrl", ($scope, FIREBASE_CONFIG) => {
-	$scope.hello = "FIREBASE_CONFIG";
+app.controller("addressCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+
+	let retrieveFBAddresses = () => {
+		let finalAddresses = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/addresses.json`)
+			.then(fbAddresses => {
+				fbAddresses = fbAddresses.data;
+				Object.keys(fbAddresses).forEach(address => {
+					fbAddresses[address].id = address;
+					finalAddresses.push(fbAddresses[address]);
+					resolve(finalAddresses);
+				});
+			})
+			.catch(error => {reject(error);});
+		});
+	};
+
+	let getAddresses = () => {
+		retrieveFBAddresses()
+		.then(addresses => {$scope.addresses = addresses;})
+		.catch(error => {console.log("Error in getAddresses", error);});
+	};
+
+	$scope.hello = "";
+	$scope.addresses = [];
+
+	getAddresses();
+
 });
 
